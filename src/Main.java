98edb7ad2e5b8bf81cb7a8476f6361c0114e21d1
Name Gulnara <nabgulnara@mail.ru>
2023-03-30 14:33:34 +0300

    project created

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 100, deposit = 1100; // баланс счета клиента и его сумма пополнения
+        int bonus = 100;                   // бонусный рубль за каждые 100 рублей пополнения
+
+        balance = deposit > 1000 ? balance + deposit +(deposit / bonus) : balance + deposit;
+
+        System.out.println("Баланс после пополнения: "+balance);
+    }
+}

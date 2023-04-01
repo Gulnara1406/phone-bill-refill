a4213b0fd06c84cb00a2921afbbe2b0d24070bdc
Name Gulnara <nabgulnara@mail.ru>
2023-04-01 16:04:51 +0300

    code reformated

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
         int balance = 100, deposit = 1100; // баланс счета клиента и его сумма пополнения
         int bonus = 100;                   // бонусный рубль за каждые 100 рублей пополнения
 
-        balance = deposit > 1000 ? balance + deposit +(deposit / bonus) : balance + deposit;
-
-        System.out.println("Баланс после пополнения: "+balance);
+        balance = deposit > 1000 ? balance + deposit + (deposit / bonus) : balance + deposit;
+        
+        System.out.println("Баланс после пополнения: " + balance);
     }
 }
